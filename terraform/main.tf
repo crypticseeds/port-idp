6c00a0466c1b905aa603b6e317bfcbb05514af18
Femi Akinlotan <94658643+crypticseeds@users.noreply.github.com>
2024-12-15 16:40:17 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -112,7 +112,7 @@ module "irsa-ebs-csi" {
 resource "aws_eks_addon" "ebs-csi" {
   cluster_name             = module.eks.cluster_name
   addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.38.1"
+  addon_version            = "v1.28.0"
   service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
   tags = {
     "eks_addon" = "ebs-csi"
